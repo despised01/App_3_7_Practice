806a4746fe14305834660f587780f0cb051585df
Антон Левин <despised01@gmail.com>
2022-10-24 13:49:04 +0300

    Добавлено приложение для анкетирования пользователя к итоговому заданию 3.7

--- App_3_7_Practice/Program.cs
+++ App_3_7_Practice/Program.cs
@@ -6,7 +6,18 @@ namespace App_3_7_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your birthdate?");
+            var birthdate = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, you're {1} years old, your birthdate is {2}", name, age, birthdate);
+
+            Console.ReadKey();
         }
     }
 }
